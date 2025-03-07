249c87fa97cf2eafa8c17dafb9c98032abf4aff7
Saurabh Rawat <saurabhrwt2121@gmail.com>
2025-03-07 15:04:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+function searchRecipe(){
+    const searchInput = document.getElementById('searchInput').value;
+    const recipesDiv = document.getElementById('recipes');
+    const notFoundDiv = document.getElementById('notFound');
+
+    //clear previous results
+    recipesDiv.innerHTML = '';
+    notFoundDiv.style.display = 'none';
+
+    if(searchInput.trim() === ''){
+        notFoundDiv.innerHTML = 'Please enter a recipe name to search!';
+        notFoundDiv.style.display = 'block';
+        return;
+    }
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+    .then(response => response.json())
+    .then(data => {
+        if(!data.meals){
+            notFoundDiv.innerText = 'Recipe not found, please try another search!';
+            notFoundDiv.style.display = 'block';
+        }
+        else{
+            data.meals.forEach(meal => {
+               const card = document.createElement('div');
+               card.classList.add('recipe-card');
+               card.innerHTML = `
+               <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+               <h3>${meal.strMeal}</h3>
+               <p>${meal.strCategory}</p>
+               <p>${meal.strArea}</p>
+               <button onclick="viewRecipe('${meal.idMeal}')">View Recipe</button>
+               `;
+
+               recipesDiv.appendChild(card);
+            });
+        }
+    })
+}
+
+function viewRecipe(mealId){
+    const popupCard = document.getElementById('popupCard');
+    const recipeTitle = document.getElementById('recipeTitle');
+    const recipeDetails = document.getElementById('recipeDetails');
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+
+    .then(response => response.json())
+    .then(data => {
+        const meal = data.meals[0];
+        recipeTitle.innerText = meal.strMeal;
+        recipeDetails.innerText = meal.strInstructions;
+        popupCard.style.display = 'block';
+    })
+}
+
+function closeRecipe(){
+    document.getElementById('popupCard').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
